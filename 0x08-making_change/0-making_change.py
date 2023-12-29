2d1c94e8046509b8a4ced7a437c9d43420410fb9
EphraimShikanga <ephraimshikanga@gmail.com>
2023-12-29 19:39:39 +0300

    makingchanges

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Making change"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """Function that makes change from given coins"""
-    if total is 0 or total < 0:
+    """Clasic Bottom-Up dynamic programming"""
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    if coins == [] or coins is None:
-        return -1
-    remainder = total
-    coins_count = 0
-    coin_id = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while remainder > 0:
-        if remainder - sorted_coins[coin_id] >= 0:
-            remainder -= sorted_coins[coin_id]
-            coins_count += 1
-        else:
-            coin_id += 1
-    return coins_count
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
